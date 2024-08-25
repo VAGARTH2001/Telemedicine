2852116bc64ecb5155fb013ac804d4f8158e47ec
iam_vagarth <Vagarthd@gmail.com>
2024-08-25 21:01:01 +0530

    port update

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ const { default: mongoose } = require('mongoose');
 const stripe = require('stripe')('sk_test_51PesjgEivxsvCmzQ9OVNq4WmsPjHZFo3LgBiYaYUN1WTsS468HZ3OHO7Y0WIdAImHsxiyegQ8Z7Erh0TS6OtJeJj00qAoz05RB');
 const connectDB = require('./db');
 const app = express();
-const port = 3000;
+const port = process.env.PORT||3000;
 
 
 connectDB();
